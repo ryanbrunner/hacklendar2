312f5e272573038ac83b9306e77b42cb5c145bb9
Ryan Brunner <ryan@ryanbrunner.com>
2013-01-21 21:07:14 -0500

    Added user names to diary entries

---
+++ db/migrate/20130122020317_remove_activity_from_diary_entry.rb
@@ -0,0 +1,7 @@
+class RemoveActivityFromDiaryEntry < ActiveRecord::Migration
+  def up
+  end
+
+  def down
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130122001121) do
+ActiveRecord::Schema.define(:version => 20130122002850) do
 
   create_table "diary_entries", :force => true do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(:version => 20130122001121) do
     t.string   "user"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+    t.string   "user_name"
   end
 
 end
